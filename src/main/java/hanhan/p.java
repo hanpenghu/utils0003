cc1d727618762eded3dbd07c7760420b024767bd
hanpenghu <563909408@qq.com>
2018-07-30 13:46:14 +0800

    2018_7_30   weekday(1)   13:46:03

--- src/main/java/hanhan/p.java
+++ src/main/java/hanhan/p.java
@@ -2124,6 +2124,43 @@ public static boolean isFirstDateBig(String firstStr,String  secondStr){
 //        p.p(p.gp().sad(p.dexhx).sad(p.strValeOf(sjc2StrDate(121344L))).sad(p.dexhx).gad());
 //        p.p(p.gp().sad(p.dexhx).sad(p.strValeOf(sjc2StrDate(121344L,d16))).sad(p.dexhx).gad());
 //    }
+
+
+
+    public static void make1970null(Object o){
+        List<Field> fieldList=new ArrayList<>();
+        Class<?> aClass = o.getClass();
+        while (aClass != null) {//用while得到所有超类的字段属性
+            fieldList.addAll(Arrays.asList(aClass.getDeclaredFields()));
+            aClass = aClass.getSuperclass(); //得到父类,然后赋给自己
+        }
+        for(Field field:fieldList){
+            field.setAccessible(true);
+            //得到当前字段类型
+            String typeName = field.getType().getTypeName();
+            if("java.util.Date".equals(typeName)){
+                //得到我的生日
+                try {
+                    if(field.get(o)!=null){
+                        Date date=null;
+                        try {  date = new SimpleDateFormat("yyyy-MM-dd").parse("1986-12-26"); } catch (ParseException e) { e.printStackTrace(); }
+                        long mybrith = date.getTime();
+                        long time = ((Date) field.get(o)).getTime();
+                        if(time<=mybrith){
+                            //如果这个时间小于我的生日,证明这个时间是在1986年以前的时间,需要变成NULL
+                            field.set(o,null);
+                        }
+                    }
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+
+
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     /**
