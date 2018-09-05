6cdbc13b11cdb0b7e55e4d24e020572c8ee1d0df
hanpenghu <563909408@qq.com>
2018-09-05 20:44:49 +0800

    2018_9_5   weekday(3)   20:43:50

--- src/main/java/hanhan/pI.java
+++ src/main/java/hanhan/pI.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public interface pI {
+	String unixTimeOri="1970-01-01 08:00:00.000";
      String[] hexDigits = { "0", "1", "2", "3", "4", "5",
             "6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };//md5用
 
