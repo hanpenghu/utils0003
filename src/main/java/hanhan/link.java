896389a733d6328c2ae39b044a26919b1cefc19a
hanpenghu <563909408@qq.com>
2018-09-11 14:05:14 +0800

    2018_9_11   weekday(2)   14:05:08

---
+++ src/main/java/hanhan/link.java
@@ -0,0 +1,37 @@
+package hanhan;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018/9/11.
+ */
+@SuppressWarnings("unchecked")
+public  class link<T>{
+
+   private List<T> list=new LinkedList<T>();
+
+    public static <T>link b(){
+        return new link<T>();
+    }
+
+    public link<T> a(T t){
+        list.add(t);
+        return this;
+    }
+
+    public List g() {
+        return list;
+    }
+
+
+
+    public static void main(String[]args){
+        List<String> g = link.b().a("1").a("2").g();
+        System.out.println(g);
+
+    }
+
+
+
+}
